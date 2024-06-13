1a6b3aa1555e22c4193ee1baf986c62cbe0d7ee3
seong bae <wltjdqo55@gamil.com>
2024-06-13 09:56:21 +0900

    feat / 240613 / created 콘솔 오류 수정 해결

--- src/main/resources/static/dist/js/toggles-list-filter.js
+++ src/main/resources/static/dist/js/toggles-list-filter.js
@@ -1,22 +1,22 @@
 // @TOGGLES
 // LIST FILTER
-const listFilterToggleBtn = document.querySelector('.btn-active-filter-position');
-
-const listFilter = document.querySelector('.list-filter');
-const listFilterClassValue = 'filter-top';
-
-const listFilterDiv = document.querySelector('.content-list > div');
-const listFilterDivClassValue = 'flex-column';
-
-if(listFilterToggleBtn != null) {
-    listFilterToggleBtn.addEventListener('click', function () {
-        setCookie('listFilterToggle', listFilter.classList.toggle(listFilterClassValue), monthInSecond);
-        setCookie('listFilterToggle', listFilterDiv.classList.toggle(listFilterDivClassValue), monthInSecond);
-    });
-}
-
-$(document).ready(function () {
-    let isToggle = getCookie('listFilterToggle');
-    if (listFilterToggleBtn != null && isToggle=='true') listFilter.classList.add(listFilterClassValue);
-    if (listFilterToggleBtn != null && isToggle=='true') listFilterDiv.classList.add(listFilterDivClassValue);
-});
+//const listFilterToggleBtn = document.querySelector('.btn-active-filter-position');
+//
+//const listFilter = document.querySelector('.list-filter');
+//const listFilterClassValue = 'filter-top';
+//
+//const listFilterDiv = document.querySelector('.content-list > div');
+//const listFilterDivClassValue = 'flex-column';
+//
+//if(listFilterToggleBtn != null) {
+//    listFilterToggleBtn.addEventListener('click', function () {
+//        setCookie('listFilterToggle', listFilter.classList.toggle(listFilterClassValue), monthInSecond);
+//        setCookie('listFilterToggle', listFilterDiv.classList.toggle(listFilterDivClassValue), monthInSecond);
+//    });
+//}
+//
+//$(document).ready(function () {
+//    let isToggle = getCookie('listFilterToggle');
+//    if (listFilterToggleBtn != null && isToggle=='true') listFilter.classList.add(listFilterClassValue);
+//    if (listFilterToggleBtn != null && isToggle=='true') listFilterDiv.classList.add(listFilterDivClassValue);
+//});
